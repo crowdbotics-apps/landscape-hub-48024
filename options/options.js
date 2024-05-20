2129c29fb66a3e612947ed964698f1a84d0db33d
Crowdbotics <team@crowdbotics.com>
2024-05-20 16:07:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "landscape-hub-48024",
+  url: "https://landscape-hub-48024.botics.co",
+  api: "https://landscape-hub-48024.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
